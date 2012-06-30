d91ba072d2588e21558f8f19ad415013a86d76e8
Venkat Naidu <naiduv@bittorrent.com>
2012-06-29 20:59:50 -0700

    split jsonstr to .js

---
+++ jsonstr.js
@@ -0,0 +1,9 @@
+var jsonstr = {"bindings": [
+    {"name": "Lebron James", "sign": "Capricorn", "wiki": "http://en.wikipedia.org/wiki/Lebron_James", "image": "./lebronjames.png"},
+    {"name": "Adolf Hitler", "sign": "Taurus", "wiki": "http://en.wikipedia.org/wiki/Adolf_Hitler", "image": "./adolfhitler.png"},
+    {"name": "Barack Obama", "sign": "Leo", "wiki": "en.wikipedia.org/wiki/Barack_Obama", "image": "./barackobama.png"},    
+    {"name": "Michael Jackson", "sign": "Virgo", "wiki": "http://en.wikipedia.org/wiki/Michael_Jackson", "image": "./michaeljackson.png"},
+    {"name": "Kim Kardashian", "sign": "Libra", "wiki": "http://en.wikipedia.org/wiki/Kim_Kardashian", "image": "./kimkardashian.png"},	
+    {"name": "Marilyn Monroe", "sign": "Gemini", "wiki": "http://en.wikipedia.org/wiki/Marilyn_Monroe", "image": "./marilynmonroe.png"},	
+    ]
+};
