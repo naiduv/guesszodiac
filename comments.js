728107558b380a1b33cf511e4647064601f384d1
naiduv <naiduv@bittorrent.com>
2012-07-02 23:02:25 +0000

    wip

--- comments.js
+++ comments.js
@@ -1,3 +1,1 @@
-var comments = {"bindings": [{"comment":"<commentstr><p>good work<p></commentstr>"},
-{"comment":"<commentstr><p>hahaha<p></commentstr>"},
-{"comment":"<commentstr><p>lololol<p></commentstr>"},]};
+var comments = {"bindings": [{"comment":"<commentstr><p>nice <p></commentstr>"},]};
