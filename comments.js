b2ac3394c021d959cd75c8148640e1bd8597941c
naiduv <naiduv@bittorrent.com>
2012-07-02 02:49:16 +0000

    wop

--- comments.js
+++ comments.js
@@ -1,3 +1,9 @@
 var comments = {"bindings": [
 
-{"comment":"<commentstr><p>nice<p></commentstr>"},]};
+{"comment":"<commentstr><p>nice<p></commentstr>"},
+{"comment":"<commentstr><p>ok thats cool<p></commentstr>"},
+{"comment":"<commentstr><p>ya man<p></commentstr>"},
+{"comment":"<commentstr><p>asd<p></commentstr>"},
+{"comment":"<commentstr><p>newstr<p></commentstr>"},
+{"comment":"<commentstr><p>asdf<p></commentstr>"},
+{"comment":"<commentstr><p>asdf<p></commentstr>"},]};
