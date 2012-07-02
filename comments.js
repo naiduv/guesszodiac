eb582a8a02a3d28fd807edbaa26ac9a6a6eb7435
naiduv <naiduv@bittorrent.com>
2012-07-02 14:27:42 +0000

    wip- works WELL

--- comments.js
+++ comments.js
@@ -1,2 +1,2 @@
 var comments = {"bindings": [{"comment":"<commentstr><p>good work<p></commentstr>"},
-{"comment":"<commentstr><p>yoyoyo<p></commentstr>"},]};
+{"comment":"<commentstr><p>what u?<p></commentstr>"},]};
--- test.cpp
+++ test.cpp
@@ -105,7 +105,7 @@ int main( int argc, char *argv[] )
          return(0);
        }
 
-     close(newsockfd);
+     //     close(newsockfd);
      cout<<"\n closing socket \n";
     }
 
